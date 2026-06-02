d53125980f32eb164e20b426bd6285c893807f6e
Quique <quique.grases@gmail.com>
2026-06-02 22:38:21 +0200

    Improving and fixing some stuff in DraggableByPath script

--- Assets/AssetStore/Keetzap/ZLDMaker/Scripts/GamePlay/Editor/Other/DraggableByPathInspector.cs
+++ Assets/AssetStore/Keetzap/ZLDMaker/Scripts/GamePlay/Editor/Other/DraggableByPathInspector.cs
@@ -11,24 +11,32 @@ namespace Keetzap.ZeldaMaker
         private DraggableByPath draggableByPath;
 
         private SerializedProperty pushTimeThreshold;
-        private SerializedProperty targetPosition;
+        private SerializedProperty targetPositionMode;
+        private SerializedProperty targetTransform;
+        private SerializedProperty targetPositionVector;
         private SerializedProperty lookAtTarget;
         private SerializedProperty offsetLookAtTarget;
         private SerializedProperty draggableRenderer;
         private SerializedProperty allowBackwards;
         private SerializedProperty lockAtTheEnd;
+        private SerializedProperty useSnap;
+        private SerializedProperty snapValue;
 
         void OnEnable()
         {
             draggableByPath = (DraggableByPath)target;
 
             pushTimeThreshold = serializedObject.FindProperty(DraggableByPath.Fields.PushTimeThreshold);
-            targetPosition = serializedObject.FindProperty(DraggableByPath.Fields.TargetPosition);
+            targetPositionMode = serializedObject.FindProperty(DraggableByPath.Fields.TargetPositionMode);
+            targetTransform = serializedObject.FindProperty(DraggableByPath.Fields.TargetTransform);
+            targetPositionVector = serializedObject.FindProperty(DraggableByPath.Fields.TargetPositionVector);
             lookAtTarget = serializedObject.FindProperty(DraggableByPath.Fields.LookAtTarget);
             offsetLookAtTarget = serializedObject.FindProperty(DraggableByPath.Fields.OffsetLookAtTarget);
             draggableRenderer = serializedObject.FindProperty(DraggableByPath.Fields.DraggableRenderer);
             allowBackwards = serializedObject.FindProperty(DraggableByPath.Fields.AllowBackwards);
             lockAtTheEnd = serializedObject.FindProperty(DraggableByPath.Fields.LockAtTheEnd);
+            useSnap = serializedObject.FindProperty(DraggableByPath.Fields.UseSnap);
+            snapValue = serializedObject.FindProperty(DraggableByPath.Fields.SnapValue);
         }
 
         public override void OnInspectorGUI()
@@ -52,41 +60,98 @@ namespace Keetzap.ZeldaMaker
         private void SectionMainProperties()
         {
             EditorGUILayout.PropertyField(pushTimeThreshold);
-            EditorGUILayout.PropertyField(targetPosition);
-            EditorGUILayout.Space(2);
-            if (targetPosition.objectReferenceValue != null)
-            {
-                EditorGUILayout.PropertyField(allowBackwards, new GUIContent("Allow reverse direction ***"));
-                //EditorGUI.BeginDisabledGroup(allowBackwards.boolValue == false);
-                //{
-                //    EditorGUI.indentLevel++;
-                //    EditorGUILayout.PropertyField(allowGoBeyondInitPos, new GUIContent("Allow surpass init position"));
-                //    EditorGUI.indentLevel--;
-                //}
-                //EditorGUI.EndDisabledGroup();
-                EditorGUILayout.PropertyField(lockAtTheEnd, new GUIContent("Lock at destination"));
+            EditorGUILayout.PropertyField(targetPositionMode);
 
-                if (draggableByPath.GetComponentInParent())
+            var mode = (DraggableByPath.TargetPositionMode)targetPositionMode.enumValueIndex;
+
+            if (mode == DraggableByPath.TargetPositionMode.Transform)
+            {
+                EditorGUILayout.PropertyField(targetTransform);
+
+                bool hasTarget = targetTransform.objectReferenceValue != null;
+                if (!hasTarget)
                 {
-                    EditorGUILayout.PropertyField(lookAtTarget, new GUIContent("Look at target ???"));
-                    EditorGUI.BeginDisabledGroup(lookAtTarget.boolValue == false);
-                    {
-                        EditorGUI.indentLevel++;
-                        EditorGUILayout.PropertyField(draggableRenderer);
-                        if (lookAtTarget.boolValue && draggableRenderer.objectReferenceValue == null)
-                        {
-                            EditorGUILayout.HelpBox("A model must be assigned.", MessageType.Warning, true);
-                        }
-                        EditorGUILayout.IntSlider(offsetLookAtTarget, 0, 360, new GUIContent("Offset in Degrees ???"));
-                        EditorGUI.indentLevel--;
-                    }
-                    EditorGUI.EndDisabledGroup();
+                    EditorGUILayout.HelpBox("A Target Transform must be assigned!", MessageType.Warning, true);
+                    return;
                 }
-                else
-                    EditorGUILayout.HelpBox("To prevent an infinite cycle, 'Look at' parameters are not accesible.\nTry unparenting the Target Position from the gameObject.", MessageType.Warning, true);
             }
             else
-                EditorGUILayout.HelpBox("A Target Position must be assigned!", MessageType.Warning, true);
+            {
+                EditorGUILayout.PropertyField(targetPositionVector, new GUIContent("Target Offset (Local)"));
+                EditorGUILayout.PropertyField(useSnap, new GUIContent("Enable Snapping"));
+                if (useSnap.boolValue)
+                {
+                    EditorGUI.indentLevel++;
+                    EditorGUILayout.PropertyField(snapValue, new GUIContent("Snap Amount"));
+                    EditorGUI.indentLevel--;
+                }
+            }
+
+            EditorGUILayout.Space(2);
+
+            EditorGUILayout.PropertyField(allowBackwards, new GUIContent("Allow reverse direction"));
+            EditorGUILayout.PropertyField(lockAtTheEnd, new GUIContent("Lock at destination"));
+
+            if (mode == DraggableByPath.TargetPositionMode.LocalOffset || (mode == DraggableByPath.TargetPositionMode.Transform && draggableByPath.GetComponentInParent()))
+            {
+                EditorGUI.BeginChangeCheck();
+                EditorGUILayout.PropertyField(lookAtTarget, new GUIContent("Look at target"));
+                if (EditorGUI.EndChangeCheck())
+                {
+                    if (!lookAtTarget.boolValue && draggableRenderer.objectReferenceValue != null)
+                    {
+                        GameObject rendererObj = (GameObject)draggableRenderer.objectReferenceValue;
+                        Undo.RecordObject(rendererObj.transform, "Reset Rotation");
+                        rendererObj.transform.localRotation = Quaternion.identity;
+                    }
+                }
+                EditorGUI.BeginDisabledGroup(lookAtTarget.boolValue == false);
+                {
+                    EditorGUI.indentLevel++;
+                    EditorGUILayout.PropertyField(draggableRenderer);
+                    if (lookAtTarget.boolValue && draggableRenderer.objectReferenceValue == null)
+                    {
+                        EditorGUILayout.HelpBox("A model must be assigned.", MessageType.Warning, true);
+                    }
+                    EditorGUILayout.IntSlider(offsetLookAtTarget, 0, 360, new GUIContent("Offset in Degrees"));
+                    EditorGUI.indentLevel--;
+                }
+                EditorGUI.EndDisabledGroup();
+            }
+            else if (mode == DraggableByPath.TargetPositionMode.Transform && !draggableByPath.GetComponentInParent())
+            {
+                EditorGUILayout.HelpBox(
+                    "To prevent an infinite cycle, 'Look at' parameters are not accessible.\n" +
+                    "Try unparenting the Target Position from the gameObject.",
+                    MessageType.Warning, true);
+            }
+        }
+
+        private void OnSceneGUI()
+        {
+            if (Application.isPlaying) return;
+            if (draggableByPath.TargetMode != DraggableByPath.TargetPositionMode.LocalOffset) return;
+
+            Transform objTransform = draggableByPath.transform;
+            Vector3 worldPos = objTransform.TransformPoint(draggableByPath.TargetPositionOffset);
+
+            EditorGUI.BeginChangeCheck();
+            Vector3 newWorldPos = Handles.PositionHandle(worldPos, objTransform.rotation);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(draggableByPath, "Move Target Position");
+                
+                Vector3 newLocalPos = objTransform.InverseTransformPoint(newWorldPos);
+                
+                if (draggableByPath.UseSnap && draggableByPath.SnapValue > 0f)
+                {
+                    newLocalPos.x = Mathf.Round(newLocalPos.x / draggableByPath.SnapValue) * draggableByPath.SnapValue;
+                    newLocalPos.y = Mathf.Round(newLocalPos.y / draggableByPath.SnapValue) * draggableByPath.SnapValue;
+                    newLocalPos.z = Mathf.Round(newLocalPos.z / draggableByPath.SnapValue) * draggableByPath.SnapValue;
+                }
+                
+                draggableByPath.TargetPositionOffset = newLocalPos;
+            }
         }
     }
 }
--- Assets/AssetStore/Keetzap/ZLDMaker/Scripts/GamePlay/Runtime/Other/DraggableByPath.cs
+++ Assets/AssetStore/Keetzap/ZLDMaker/Scripts/GamePlay/Runtime/Other/DraggableByPath.cs
@@ -8,29 +8,48 @@ namespace Keetzap.ZeldaMaker
     [RequireComponent(typeof(Rigidbody))]
     public class DraggableByPath : MonoBehaviour, IDraggable
     {
+        public enum TargetPositionMode { Transform, LocalOffset }
         public static class Fields
         {
             public static string PushTimeThreshold => nameof(pushTimeThreshold);
-            public static string TargetPosition => nameof(targetPosition);
+            public static string TargetPositionMode => nameof(targetPositionMode);
+            public static string TargetTransform => nameof(targetTransform);
+            public static string TargetPositionVector => nameof(targetPositionVector);
             public static string LookAtTarget => nameof(lookAtTarget);
             public static string OffsetLookAtTarget => nameof(offsetLookAtTarget);
             public static string DraggableRenderer => nameof(draggableRenderer);
             public static string AllowBackwards => nameof(allowBackwards);
             public static string LockAtTheEnd => nameof(lockAtTheEnd);
+            public static string UseSnap => nameof(useSnap);
+            public static string SnapValue => nameof(snapValue);
         }
 
         [SerializeField] private float pushTimeThreshold = 0.3f;
-        [SerializeField] private GameObject targetPosition;
+        [SerializeField] private TargetPositionMode targetPositionMode = TargetPositionMode.Transform;
+        [SerializeField] private Transform targetTransform;
+        [SerializeField] private Vector3 targetPositionVector = new Vector3(0f, 0f, 1f);
         [SerializeField] private bool lookAtTarget;
         [SerializeField] private int offsetLookAtTarget;
         [SerializeField] private GameObject draggableRenderer;
         [SerializeField] private bool allowBackwards;
         [SerializeField] private bool lockAtTheEnd;
+        [SerializeField] private bool useSnap = true;
+        [SerializeField] private float snapValue = 0.5f;
+
+        public TargetPositionMode TargetMode => targetPositionMode;
+        public Vector3 TargetPositionOffset
+        {
+            get => targetPositionVector;
+            set => targetPositionVector = value;
+        }
+        public bool UseSnap => useSnap;
+        public float SnapValue => snapValue;
         
         private PlayerController _playerController;
         private BoxCollider _colliderTrigger;
         private Vector3 _initPosition;
         private Vector3 _targetPosition;
+        private Vector3 _absoluteVector3Target;
         private float _pushTimeThreshold;
         private bool _objectIsDragged;
         private bool _objectIsPushed;
@@ -47,6 +66,10 @@ namespace Keetzap.ZeldaMaker
             SetTriggerCollider();
 
             _initPosition = transform.position;
+            if (targetPositionMode == TargetPositionMode.LocalOffset)
+            {
+                _absoluteVector3Target = transform.TransformPoint(targetPositionVector);
+            }
         }
 
         private void SetTriggerCollider()
@@ -91,16 +114,25 @@ namespace Keetzap.ZeldaMaker
         {
             if (Application.isPlaying || draggableRenderer == null) return;
 
-            if (lookAtTarget && targetPosition != null && targetPosition.GetComponentInParent<DraggableByTile>() == null)
+            if (lookAtTarget)
             {
-                draggableRenderer.transform.LookAt(targetPosition.transform, Vector3.up);
-                draggableRenderer.transform.eulerAngles += new Vector3(0, offsetLookAtTarget, 0);
+                if (targetPositionMode == TargetPositionMode.Transform && targetTransform != null && targetTransform.GetComponentInParent<DraggableByTile>() == null)
+                {
+                    draggableRenderer.transform.LookAt(targetTransform, Vector3.up);
+                    draggableRenderer.transform.eulerAngles += new Vector3(0, offsetLookAtTarget, 0);
+                }
+                else if (targetPositionMode == TargetPositionMode.LocalOffset)
+                {
+                    Vector3 worldTarget = transform.TransformPoint(targetPositionVector);
+                    draggableRenderer.transform.LookAt(worldTarget, Vector3.up);
+                    draggableRenderer.transform.eulerAngles += new Vector3(0, offsetLookAtTarget, 0);
+                }
             }
         }
 
         public bool GetComponentInParent()
         {
-            return targetPosition != null && targetPosition.GetComponentInParent<DraggableByTile>() == null;
+            return targetTransform != null && targetTransform.GetComponentInParent<DraggableByTile>() == null;
         }
 
         public void OnStartDragging(PlayerController playerController)
@@ -119,16 +151,16 @@ namespace Keetzap.ZeldaMaker
 
             if (_pushTimeThreshold <= 0)
             {
-                _targetPosition = targetPosition.transform.position;
+                _targetPosition = (targetPositionMode == TargetPositionMode.Transform) ? targetTransform.position : (Application.isPlaying ? _absoluteVector3Target : transform.TransformPoint(targetPositionVector));
 
                 _pushDirection = GetPushDirectionVector(_initPosition - _playerController.transform.position);
-                _pathDirection = targetPosition.transform.position - _initPosition;
+                _pathDirection = (targetPositionMode == TargetPositionMode.Transform ? targetTransform.position : (Application.isPlaying ? _absoluteVector3Target : transform.TransformPoint(targetPositionVector))) - _initPosition;
                 _pushAngle = Functions.Remap(Vector3.Angle(_pushDirection, _pathDirection), 0, 90, 1, 0);
                 
-                if (targetPosition.transform.parent != null)
+                if (targetTransform != null && targetTransform.parent != null)
                 {
-                    Transform currentParent = targetPosition.transform.parent;
-                    targetPosition.transform.SetParent(null);
+                    Transform currentParent = targetTransform.parent;
+                    targetTransform.SetParent(null);
                 }
 
                 _colliderTrigger.center -= _pushDirection;
@@ -159,11 +191,21 @@ namespace Keetzap.ZeldaMaker
 
         void OnDrawGizmos()
         {
-            if (targetPosition != null)
+            if (targetTransform != null || targetPositionMode == TargetPositionMode.LocalOffset)
             {
                 Gizmos.color = Color.cyan;
-                Gizmos.DrawLine(transform.position, targetPosition.transform.position);
-                Gizmos.DrawSphere(targetPosition.transform.position, 0.25f);
+                
+                if (targetPositionMode == TargetPositionMode.Transform && targetTransform != null)
+                {
+                    Gizmos.DrawLine(transform.position, targetTransform.position);
+                    Gizmos.DrawSphere(targetTransform.position, 0.25f);
+                }
+                else
+                {
+                    Vector3 worldTarget = Application.isPlaying ? _absoluteVector3Target : transform.TransformPoint(targetPositionVector);
+                    Gizmos.DrawLine(transform.position, worldTarget);
+                    Gizmos.DrawSphere(worldTarget, 0.25f);
+                }
                 Gizmos.DrawSphere(Application.isPlaying ? _initPosition : transform.position, 0.25f);
             }
         }
